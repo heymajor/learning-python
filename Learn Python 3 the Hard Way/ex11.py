da3f67d8734ed4a0f88c5796af343eaab67b7640
Major Hoffman <hoffmanmajor@gmail.com>
2019-06-23 03:14:59 -0400

    Typed out ex11.py

---
+++ Learn Python 3 the Hard Way/ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("how much do you weight?", end=' ')
+weight = input()
+
+print(f"so, you're {ag} old, {height} tall and {weight} heavy.")
